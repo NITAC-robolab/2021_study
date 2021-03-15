36230bd0d439ee0b0430c7800b029b5c47da9a31
NITAC-robolab <nitac.robolab@gmail.com>
2021-03-15 16:33:18 +0900

    class etc.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
-#include<stdio.h>
+#include<sstream>
 
-int main(void)
+class yamada
 {
+    public: 
+    int x;
+    int y;
+void function();
+
+private:
+int a=2;
+    
+};
+
+void yamada::function(){
+printf("%d\n",a);
+}
+int main()
+{
+    yamada kk;
+    kk.x=60;
+    kk.y=7;
+printf("%d\n%d",kk.x,kk.y);
+kk.function();
+    
     return 0;
 }
+
