8e04318041bc60bbef0840b1e4f2bc7ed9ea5b5b
masato oohira <ohiram590@gmail.com>
2021-03-15 16:18:19 +0900

    classについて

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include<stdio.h>
 
+/*
+class my
+{
+    public:
+        size_t val_1,val_2;
+        void func();
+    private:
+        size_t val = 5;
+};
+
+void my::func()
+{
+    printf("%d\n", val);
+}
+
 int main(void)
 {
+    my oohira;
+    oohira.val_1 = 10;
+    oohira.val_2 = 20;
+
+    oohira.func();
+
     return 0;
-}
+}*/
